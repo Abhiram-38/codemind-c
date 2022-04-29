c834b124983b9d6db60588c69c85cb2fc847af70
Abhiram-38 <97424563+Abhiram-38@users.noreply.github.com>
2022-04-29 19:22:08 +0530

    #include<stdio.h> int fun(int n) {     int i,fc=0;     for(i=1;i<=n;i++)     {         if(n%i==0)         {             fc++;         }     }     if(fc==2)     {         return 1;     }     else     {         return 0;     } } int main() {     int n,a[100],i,x,c=0;     float res=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d",&x);     for(i=0;i<n;i++)     {         if(fun(a[i]))         {            if(a[i]>=x)            {                c++;            }         }     }     printf("%d",c); }

---
+++ Primes_greater_than_k.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int fun(int n)
+{
+    int i,fc=0;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            fc++;
+        }
+    }
+    if(fc==2)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int main()
+{
+    int n,a[100],i,x,c=0;
+    float res=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&x);
+    for(i=0;i<n;i++)
+    {
+        if(fun(a[i]))
+        {
+           if(a[i]>=x)
+           {
+               c++;
+           }
+        }
+    }
+    printf("%d",c);
+}
